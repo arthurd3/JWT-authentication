6651f00f2567b26a39e9375bfdc1b85ec13bdbba
arthurd3 <arthur_camposl@yahoo.com>
2025-09-07 18:13:53 -0300

    create a chain filter to validate JWT token

---
+++ security-jwt/src/main/java/com/arthur/securityjwt/security/AuthTokenFilter.java
@@ -0,0 +1,66 @@
+package com.arthur.securityjwt.security;
+
+import com.arthur.securityjwt.service.CustomUserDetailsService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@Slf4j
+public class AuthTokenFilter extends OncePerRequestFilter {
+
+    public static final String BEARER_ = "Bearer ";
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Autowired
+    private CustomUserDetailsService userDetailsService;
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain)
+            throws ServletException, IOException {
+        try{
+            String jwt = parseJwt(request);
+            if (jwt != null && jwtUtil.validateToken(jwt)) {
+                final String userName = jwtUtil.getUserFromToken(jwt);
+                final UserDetails userDetails =
+                        userDetailsService.loadUserByUsername(userName);
+
+                UsernamePasswordAuthenticationToken authenticationToken =
+                        new UsernamePasswordAuthenticationToken(
+                                userDetails,
+                                null,
+                                userDetails.getAuthorities()
+                        );
+
+                authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+
+            }
+
+        }
+    }
+
+    private String parseJwt(final HttpServletRequest request){
+        String header = request.getHeader("Authorization");
+        if(header != null && header.startsWith(BEARER_)){
+            return header.substring(BEARER_.length());
+        }
+        return null;
+    }
+}
